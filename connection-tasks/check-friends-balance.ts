4448ff29002fa12bccf79b0f1be618106e94b7de
Roman Matviitsiv <Roman_Matviitsiv@epam.com>
2024-07-24 16:17:30 +0200

    Add connection tasks

---
+++ connection-tasks/check-friends-balance.ts
@@ -0,0 +1,26 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const suppliedPublicKey = process.argv[2];
+if (!suppliedPublicKey) {
+    throw new Error("Provide a public key to check the balance of!");
+}
+
+const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+
+try {
+    const publicKey = new PublicKey(suppliedPublicKey);
+
+    const balanceInLamports = await connection.getBalance(publicKey);
+
+    const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+    console.log(
+        `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL} SOL!`,
+    );
+} catch (error) {
+    if (error instanceof TypeError) {
+        console.error("❌ Error: The provided public key is invalid.");
+    } else {
+        console.error(`❌ An unexpected error occurred: ${error.message}`);
+    }
+}
